926f789b8a26086850027cf7f3488087e510d332
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-08-06 19:03:16 +0300

    + fixed

--- Hotel/Hotel.Web/App_Start/RouteConfig.cs
+++ Hotel/Hotel.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Hotel.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Reservation", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
