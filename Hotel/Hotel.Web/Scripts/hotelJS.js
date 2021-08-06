a9a8cad310215782df3c497663a412270774a88d
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-08-06 18:13:22 +0300

    + update views

--- Hotel/Hotel.Web/Scripts/hotelJS.js
+++ Hotel/Hotel.Web/Scripts/hotelJS.js
@@ -1,4 +1,20 @@
 ﻿$(document).ready(function () {
     if ($(document).height() <= $(window).height())
-        $("footer.footer").addClass("navbar-fixed-bottom");
+        $('footer.footer').addClass('navbar-fixed-bottom');
+
+    $('.input-daterange').datepicker({
+        format: 'dd-mm-yyyy',
+        autoclose: true,
+        calendarWeeks: true,
+        clearBtn: true,
+        disableTouchKeyboard: true
+    });
+
+    $('#start').click(function () {
+        $('#fa-2').hide();
+    });
+    $('#end').click(function () {
+        $('#fa-1').hide();
+    });
+
 });
